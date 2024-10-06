50fee3a7b6a83a754c825df8445a24ded33c4c20
dostonlv <dostonlv@icloud.com>
2024-10-06 20:20:42 +0500

    fs #1 (create)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+// create function to create files/fresh.txt and fresh.txt inside -> I am fresh and young 
+// if file already exists then it should not overwrite the file
+
+
+// create file
+import { join,dirname } from 'node:path';
+import promises from 'node:fs/promises';
+import { fileURLToPath } from "node:url";
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 const create = async () => {
-    // Write your code here 
+    const fileName = join(__dirname,'files','fresh.txt');
+    const content = 'I am fresh and young';
+
+    try {
+        await promises.access(fileName);
+        console.log('File already exists');
+    }
+    catch {
+        await promises.writeFile(fileName, content);
+        console.log('File created');
+    }
+
 };
 
 await create();
