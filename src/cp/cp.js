b654445cc476f52271294fa5eef8f0c1fbb11424
dostonlv <dostonlv@icloud.com>
2024-10-07 12:02:20 +0500

    module  #8  done ✅

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import {execFile} from 'node:child_process';
+import { join,dirname } from 'node:path';
+import { fileURLToPath } from "node:url";
 
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const spawnChildProcess = async (args) => {
+    const filePath = join(__dirname,'files','script.js')
+   const child =  execFile("node",[filePath,...args])
+   child.stdout.on("data",(data)=>{
+    console.log(`Child process:\n${data.toString()}`);
+   })
+   child.on("exit", (code)=>{
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+   })
+   child.on("error",(err)=>{
+    console.error(`Failed to start child process: ${err.message}`);
+   })
+
+   process.stdin.on("data", (data) => {
+    child.stdin.write(data);
+  });
+
+
+};
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['kei','k3i',1,2,3,'world']);
