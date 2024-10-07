0634108fae711c79c245a4c5392233feff7d3928
dostonlv <dostonlv@icloud.com>
2024-10-07 13:59:55 +0500

    module  #7  done ✅

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,58 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from "worker_threads";
+import os from "os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 
-await performCalculations();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const performCalculations = async () => {
+  const numCores = os.cpus().length;
+  const results = new Array(numCores).fill(null);
+  let finishedWorkers = 0;
+
+  const workerPath = join(__dirname, "worker.js");
+
+  const promises = Array.from({ length: numCores }, (_, i) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(workerPath, {
+        workerData: 10 + i, 
+      });
+  
+      results[i] = { status: null, data: null };
+  
+      worker.on("message", (message) => {
+
+        results[i] = { status: "resolved", data: message };
+  
+        finishedWorkers += 1;
+  
+
+        if (finishedWorkers === numCores) {
+          console.log('Final Results:', results); 
+          resolve({ status: "success", data: results });
+        }
+      });
+  
+      worker.on("error", (error) => {
+        console.error(`Worker error: ${error}`);
+  
+
+        results[i] = { status: "error", data: null };
+  
+        finishedWorkers += 1;
+  
+        if (finishedWorkers === numCores) {
+          console.log('Final Results (with errors):', results);
+          resolve({ status: "error", data: results });
+        }
+      });
+    });
+  });
+  
+
+  const final = await Promise.all(promises);
+  console.log(final);
+}
+
+performCalculations();
