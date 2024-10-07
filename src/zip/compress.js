a1c99f2d434b35bcc02bc1b227969242eacb243b
dostonlv <dostonlv@icloud.com>
2024-10-07 10:59:25 +0500

    module  #6 done ✅

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { join,dirname } from 'node:path';
+import { createGzip } from 'node:zlib';
+import { fileURLToPath } from "node:url";
+import fs from 'node:fs';
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+   const filePath = join(__dirname,'files','fileToCompress.txt');
+   const compressedFilePath = join(__dirname,'files','archive.gz');
+    const file =  fs.createReadStream(filePath, {encoding: 'utf-8'});
+
+    const compressedFile = fs.createWriteStream(compressedFilePath);
+    const gzip = createGzip();
+    file.pipe(gzip).pipe(compressedFile);
+    console.log('File compressed');
+
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { join,dirname } from 'node:path';
+import  zlib  from 'node:zlib';
+import { fileURLToPath } from "node:url";
+import fs from 'node:fs';
+import stream from 'node:stream/promises';
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 const decompress = async () => {
-    // Write your code here 
+    const compressedFilePath = join(__dirname,'files','archive.gz');
+    const decompressedFilePath = join(__dirname,'files','fileToCompress.txt');
+
+    const compressedFile =  fs.createReadStream(compressedFilePath);
+    const decompressedFile = fs.createWriteStream(decompressedFilePath);
+
+    const unzip =  zlib.createUnzip()
+
+      stream.pipeline(compressedFile,unzip,decompressedFile)
+
 };
 
 await decompress();
